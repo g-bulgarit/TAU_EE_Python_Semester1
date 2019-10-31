59e953aedcaef834bc5b860888b1b19509ecb72c
g-bulgarit <alonio6@gmail.com>
2019-10-31 21:57:50 +0200

    original HW1 file

---
+++ HW1/ex1_315781666.py
@@ -0,0 +1,48 @@
+''' Exercise #1 - Python.'''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+R = ??? # Replace ??? with a positive float of your choice.
+# Write the rest of the code for question 1 below here.
+
+
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+S = ??? # Replace ??? a string of your choice.
+# Write the rest of the code for question 2 below here.
+
+
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+number  = ??? # Replace ??? with a int of your choice.
+# Write the rest of the code for question 3 below here.
+
+
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+a = ??? # Replace ??? with a positive int of your choice.
+b = ???  # Replace ??? with a positive int of your choice.
+c = ???  # Replace ??? with a positive int of your choice.
+# Write the rest of the code for question 4 below here.
+
+
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+year = ??? # Replace ??? with a positive int of your choice.
+# Write the rest of the code for question 5 below here.
+
+
+
