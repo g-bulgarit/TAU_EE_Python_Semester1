8cd806cbdd678a2980cab2a8b1da45618d0f1190
g-bulgarit <alonio6@gmail.com>
2019-10-31 22:03:51 +0200

    Finished Q1

--- HW1/ex1_315781666.py
+++ HW1/ex1_315781666.py
@@ -3,16 +3,23 @@
 #########################################
 # Question 1 - do not delete this comment
 #########################################
-R = ??? # Replace ??? with a positive float of your choice.
+R = 5 # Replace ??? with a positive float of your choice.
 # Write the rest of the code for question 1 below here.
 
+pi = 3.14 # we define pi like this...
+circle_diameter = 2*R
+circle_circumference = 2*pi*R
+circle_area = pi*(R**2)
 
-
+# Print the required stuff...
+print("Diameter is: " +str(circle_diameter))
+print("Circumference is: " +str(circle_circumference))
+print("Area is: " +str(circle_area))
 
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-S = ??? # Replace ??? a string of your choice.
+S = "A string of my choice" # Replace ??? a string of your choice.
 # Write the rest of the code for question 2 below here.
 
 
@@ -21,7 +28,7 @@ S = ??? # Replace ??? a string of your choice.
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-number  = ??? # Replace ??? with a int of your choice.
+number  = 7 # Replace ??? with a int of your choice.
 # Write the rest of the code for question 3 below here.
 
 
@@ -30,9 +37,9 @@ number  = ??? # Replace ??? with a int of your choice.
 #########################################
 # Question 4 - do not delete this comment
 #########################################
-a = ??? # Replace ??? with a positive int of your choice.
-b = ???  # Replace ??? with a positive int of your choice.
-c = ???  # Replace ??? with a positive int of your choice.
+a = 7 # Replace ??? with a positive int of your choice.
+b = 6  # Replace ??? with a positive int of your choice.
+c = 5  # Replace ??? with a positive int of your choice.
 # Write the rest of the code for question 4 below here.
 
 
@@ -41,7 +48,7 @@ c = ???  # Replace ??? with a positive int of your choice.
 #########################################
 # Question 5 - do not delete this comment
 #########################################
-year = ??? # Replace ??? with a positive int of your choice.
+year = 1911 # Replace ??? with a positive int of your choice.
 # Write the rest of the code for question 5 below here.
 
 
