d3c2845483fcb53d7bb4553ace2860ea8233d0c3
g-bulgarit <alonio6@gmail.com>
2019-11-07 19:48:50 +0200

    Added HW2

---
+++ HW2/ex2_315781666.py
@@ -0,0 +1,68 @@
+""" Exercise #2. Python for Engineers."""
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+
+a = 3  # Replace the assignment with a positive integer to test your code.
+A = [1, 2, 3, 4, 5]  # Replace the assignment with other lists to test your code.
+
+
+# Write the code for question 2 using a for loop below here.
+
+# Write the code for question 2 using a while loop below here.
+
+
+# End of code for question 1
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+B = ['hello', 'world', 'course', 'python', 'day']
+# Replace the assignment with other lists of strings (str) to test your code.
+
+
+# Write the rest of the code for question 2 below here.
+
+
+
+# End of code for question 2
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+
+C = [0, 1, 2, 3, 4]  # Replace the assignment with other lists to test your code.
+
+
+# Write the rest of the code for question 3 below here.
+
+
+
+# End of code for question 3
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+
+D = [1, 2, 4, 7]  # Replace the assignment with other lists to test your code.
+
+# Write the rest of the code for question 4 below here.
+
+
+
+# End of code for question 4
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+
+my_string = 'abaadddefggg'  # Replace the assignment with other strings to test your code.
+k = 3  # Replace the assignment with a positive integer to test your code.
+
+# Write the rest of the code for question 5 below here.
+
+
+
+# End of code for question 5
