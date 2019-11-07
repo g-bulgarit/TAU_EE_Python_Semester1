9396a028001bf3cff821d77f6054e345ce4b5955
g-bulgarit <alonio6@gmail.com>
2019-11-07 19:59:53 +0200

    Solved Q1 and half of Q2

--- HW2/ex2_315781666.py
+++ HW2/ex2_315781666.py
@@ -7,6 +7,18 @@
 a = 3  # Replace the assignment with a positive integer to test your code.
 A = [1, 2, 3, 4, 5]  # Replace the assignment with other lists to test your code.
 
+divisible_idx = None
+for idx, number in enumerate(A):
+    if number % a == 0:
+        # Set divisible_idx as the index of num.
+        divisible_idx = idx
+        break
+
+if divisible_idx is None:
+    print("-1")
+else:
+    print(divisible_idx)
+
 
 # Write the code for question 2 using a for loop below here.
 
@@ -23,6 +35,22 @@ B = ['hello', 'world', 'course', 'python', 'day']
 
 
 # Write the rest of the code for question 2 below here.
+
+# For loop variant:
+letter_sum = 0
+for in_str in B:
+    letter_sum += len(in_str)
+
+avg_str_len = letter_sum/len(B)
+stdev_counter = 0
+
+for in_str in B:
+    if len(in_str) > avg_str_len:
+        stdev_counter += 1
+
+print("The number of strings longer than the average is: " +str(stdev_counter))
+
+# While loop variant:
 
 
 
