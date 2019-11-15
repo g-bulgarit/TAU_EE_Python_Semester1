0bf4d580dcb3e5041a4b98c69062d317efc9b75a
g-bulgarit <alonio6@gmail.com>
2019-11-15 09:04:19 +0200

    Added HW3

---
+++ HW3/ex3_315781666.py
@@ -0,0 +1,43 @@
+''' Exercise #3. Python for Engineers.'''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+def sum_divisible_by_k(lst, k):
+    pass  # replace this with your implementation
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+def mult_odd_digits(n):
+    pass  # replace this with your implementation
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+def count_longest_repetition(s, c):
+    pass  # replace this with your implementation
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+def upper_strings(lst):
+    pass  # replace this with your implementation
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+def div_mat_by_scalar(mat, alpha):
+    pass  # replace this with your implementation
+
+
+#########################################
+# Question 6 - do not delete this comment
+#########################################
+def mat_transpose(mat):
+    pass  # replace this with your implementation
+
