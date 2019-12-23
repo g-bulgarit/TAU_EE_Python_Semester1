7b74da7279dd97a4538729cf80467e90b9011bb9
g-bulgarit <alonio6@gmail.com>
2019-12-23 19:18:08 +0200

    Coin sort without memo works

--- HW6/ex6_315781666.py
+++ HW6/ex6_315781666.py
@@ -74,77 +74,72 @@ def catalan_rec(n,memo=None):
 # Question 4.a - do not delete this comment
 #########################################
 def find_num_changes_rec(n, lst):
-    if n <= 0 and len(lst) != 0:
-        return find_num_changes_rec(n+lst[-1], lst[0:-1])
-
-    if len(lst) == 0 and n != 0:
-        return 0
-
     if n == 0:
         return 1
+    elif n < 0 or not lst:
+        return 0
 
-    # Tfira:
-    if len(lst) == 1 and lst[-1] <= n:
-        return 1
-
-    # Work with example: coins = [1,2,5,6] | target = 5 | answer = 4
-    # obviously, can't use 6 because 6 is bigger than 5 (which is n)
-    # thus,
-    if lst[-1] > n:  # Last element is too big to be used!
-        return find_num_changes_rec(n, lst[:-1])  # Remove the last element of this list.
-    else:
-        # Otherwise, if n is equal or smaller than the target,
-        # Start traversing the list, adding 1 to the result as we 'used up a move'
-        return 1 + find_num_changes_rec(n-(lst[-1]), lst)
-
+    return find_num_changes_rec(n-lst[0], lst) + find_num_changes_rec(n, lst [1:])
 
 #########################################
 # Question 4.b - do not delete this comment
 #########################################
 def find_num_changes_mem(n, lst, memo=None):
-    pass #replace this with your implementation
+    if n == 0:
+        return 1
+    if not memo:
+        memo = {}
+    elif n < 0 or not lst:
+        return 0
+    key = (n, tuple(lst))
+    if key not in memo:
+        memo[tuple((n - lst[0], tuple(lst)))] = find_num_changes_mem(n - lst[0], lst, memo)
+        memo[tuple((n, tuple(lst[1:])))] = find_num_changes_mem(n, lst[1:], memo)
+
+    print(memo)
+    return memo[tuple((n - lst[0], tuple(lst)))] + memo[tuple((n, tuple(lst[1:])))]
+
 
 
 #########################
 # main code - do not delete this comment
 # You can add more validation cases below
 #########################
-"""
-#Question 1.a tests - you can and should add more    
-print(four_bonacci_rec(0) == 0)
-print(four_bonacci_rec(5) == 12)
-print(four_bonacci_rec(8) == 85)
 
-#Question 1.b tests - you can and should add more    
-print(four_bonacci_mem(0) == 0)
-print(four_bonacci_mem(5) == 12)
-print(four_bonacci_mem(8) == 85)
-print(four_bonacci_mem(100) == 14138518272689255365704383960)
-#Question 2 tests - you can and should add more
-print(climb_combinations_memo(4) == 5)
-print(climb_combinations_memo(42) == 433494437)
+#Question 1.a tests - you can and should add more    
+# print(four_bonacci_rec(0) == 0)
+# print(four_bonacci_rec(5) == 12)
+# print(four_bonacci_rec(8) == 85)
+#
+# #Question 1.b tests - you can and should add more
+# print(four_bonacci_mem(0) == 0)
+# print(four_bonacci_mem(5) == 12)
+# print(four_bonacci_mem(8) == 85)
+# print(four_bonacci_mem(100) == 14138518272689255365704383960)
+# #Question 2 tests - you can and should add more
+# print(climb_combinations_memo(4) == 5)
+# print(climb_combinations_memo(42) == 433494437)
 
 #Question 3 tests - you can and should add more    
-cat_list = [1,1,2,5,14,42,132,429]
-print(catalan_rec(3))
-for n,res in enumerate(cat_list):
-    print(catalan_rec(n) == res)
-"""
-#Question 4.a tests - you can and should add more        
-print(find_num_changes_rec(5,[1,2,5,6]) == 4)
-print(find_num_changes_rec(4,[1,2,5,6]) == 3)
-print(find_num_changes_rec(0.9,[1,2,5,6]) == 0)
-print(find_num_changes_rec(1,[2,5,6]) == 0)
-print(find_num_changes_rec(-1,[1,2,5,6]) == 0)
-print(find_num_changes_rec(2,[0.5,1]) == 3)
-print(find_num_changes_rec(2,[]) == 0)
-print(find_num_changes_rec(0,[]) == 1)
-print(find_num_changes_rec(0,[1]) == 1)
-print(find_num_changes_rec(0,[1]))
+# cat_list = [1,1,2,5,14,42,132,429]
+# print(catalan_rec(3))
+# for n,res in enumerate(cat_list):
+#     print(catalan_rec(n) == res)
+# #
+# # #Question 4.a tests - you can and should add more
+# print(find_num_changes_rec(5,[1,2,5,6]) == 4)
+# print(find_num_changes_rec(4,[1,2,5,6]) == 3)
+# print(find_num_changes_rec(4,[1,2,5,6]))
+# print(find_num_changes_rec(0.9,[1,2,5,6]) == 0)
+# print(find_num_changes_rec(1,[2,5,6]) == 0)
+# print(find_num_changes_rec(-1,[1,2,5,6]) == 0)
+# print(find_num_changes_rec(2,[0.5,1]) == 3)
 
-#Question 4.b tests - you can and should add more        
-#print(find_num_changes_mem(5,[1,2,5,6]) == 4)
-#print(find_num_changes_mem(4,[1,2,5,6]) == 3)
-#print(find_num_changes_mem(0.9,[1,2,5,6]) == 0)
+
+# Question 4.b tests - you can and should add more
+print(find_num_changes_mem(5,[1,2,5,6]) == 4)
+print(find_num_changes_mem(4,[1,2,5,6]) == 3)
+print(find_num_changes_mem(900,[1,2,5,6,13]))
+# print(find_num_changes_mem(0.9,[1,2,5,6]) == 0)
 
 # ============================== END OF FILE =================================
