a4b058dde084b50fb11cda7005a4f4c4ca19139b
g-bulgarit <alonio6@gmail.com>
2019-12-26 21:38:27 +0200

    Q1 finished, added test case too but mostly tested in console..

--- HW7/ex7_315781666.py
+++ HW7/ex7_315781666.py
@@ -7,12 +7,28 @@
 
 
 class Beverage:
+	name = None
+	price = None
+	is_diet = None
 	
 	def __init__(self, name, price, is_diet):
-		pass
-		
-	def get_final_price(self, size):
-		pass
+		self.name = name
+		if price > 0:
+			self.price = price
+		else:
+			raise ValueError("Prices can't be negative")
+		self.is_diet = is_diet
+
+	def get_final_price(self, size="Large"):
+		if size == "Large":
+			return self.price
+		if size == "XL":
+			return self.price * 1.25
+		if size == "Normal":
+			return self.price * 0.75
+		else:
+			raise ValueError("No such size exists.")
+
 
 #########################################
 # Question 2 - do not delete this comment
@@ -49,3 +65,8 @@ class Meal:
 	
 	def is_healthy(self):
 		pass
+
+
+# Tests
+diet_coke = Beverage("Coca Cola", 15, True)
+pass
