7cee06121dc52d7dc55a8d30db2977e71a548f6f
g-bulgarit <alonio6@gmail.com>
2019-12-26 21:32:43 +0200

    Added HW7

---
+++ HW7/ex7_315781666.py
@@ -0,0 +1,51 @@
+""" Exercise #7. Python for Engineers."""
+
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+
+
+class Beverage:
+	
+	def __init__(self, name, price, is_diet):
+		pass
+		
+	def get_final_price(self, size):
+		pass
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+
+
+class Pizza:
+	
+	def __init__(self, name, price, calories, toppings):
+		pass
+	
+	def get_final_price(self, size):
+		pass
+	
+	def add_topping(self, topping, calories, price):
+		pass
+	
+	def remove_topping(self, topping, calories, price):
+		pass
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+
+
+class Meal:
+	
+	def __init__(self, beverage, pizza):
+		pass
+	
+	def get_final_price(self, beverage_size, pizza_size):
+		pass
+	
+	def is_healthy(self):
+		pass
