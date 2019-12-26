76d2c1838b6d2e30fb14568f8d1926d7f34c3d4a
g-bulgarit <alonio6@gmail.com>
2019-12-26 21:50:33 +0200

    Finished Q2

--- HW7/ex7_315781666.py
+++ HW7/ex7_315781666.py
@@ -36,18 +36,51 @@ class Beverage:
 
 
 class Pizza:
-	
+	name = None
+	price = None
+	calories = None
+	toppings = []
+
 	def __init__(self, name, price, calories, toppings):
-		pass
+		self.name = name
+		if price > 0:
+			self.price = price
+		else:
+			raise ValueError("Price can't be negative")
+		if calories > 0:
+			self.calories = calories
+		else:
+			raise ValueError("Calories can't be negative")
+		if toppings:
+			self.toppings.extend(toppings)
 	
-	def get_final_price(self, size):
-		pass
+	def get_final_price(self, size="Family"):
+		if size == "Family":
+			return self.price
+		if size == "XL":
+			return self.price * 1.25
+		if size == "Personal":
+			return self.price * 0.6
+		else:
+			raise ValueError("Invalid size")
 	
 	def add_topping(self, topping, calories, price):
-		pass
+		if topping not in self.toppings:
+			self.toppings.extend(topping)
+			self.calories += calories
+			self.price += price
+		else:
+			raise ValueError("Topping already exists on the pizza")
 	
 	def remove_topping(self, topping, calories, price):
-		pass
+		if topping in self.toppings:
+			self.toppings.remove(topping)
+			self.calories -= calories
+			self.price -= price
+			if self.price < 0 or self.calories < 0:
+				raise ValueError("Calories or Price became negative...")
+		else:
+			raise ValueError("Topping already exists on the pizza")
 
 
 #########################################
@@ -69,4 +102,5 @@ class Meal:
 
 # Tests
 diet_coke = Beverage("Coca Cola", 15, True)
+my_pizza = Pizza("Four Cheese", 50, 1200, ['Mozza', 'Gouda', 'Goat Cheese', 'Parm'])
 pass
