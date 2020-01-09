51feeadaf85173b56ee0b0106a32426d378d7e57
g-bulgarit <alonio6@gmail.com>
2020-01-09 18:40:39 +0200

    Added HW9

---
+++ HW9/ex9.py
@@ -0,0 +1,57 @@
+''' Exercise #9. Python for Engineers.'''
+
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+
+def load_training_data(filename):
+    pass
+
+
+def get_highest_weight_loss_trainee(data, column_names, row_names):
+    pass
+
+
+def get_diff_data(data, column_names, row_names):
+    pass
+
+
+def get_highest_loss_month(data, column_names, row_names):
+    pass
+
+
+def get_relative_diff_table(data, column_names, row_names):
+    pass
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+
+
+def read_missions_file(file_name):
+    pass
+
+
+def sum_rewards(bounties):
+    pass
+
+
+def find_best_kingdom(kingdoms, bounties):
+    pass
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+def compute_entropy(img):
+    pass
+
+
+def nearest_enlarge(img, a):
+    pass
